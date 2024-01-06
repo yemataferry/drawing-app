3a2f945893705ebc5d4bd51e429acda2cfdd7414
Yemata Ferry <yemataferry@gmail.com>
2024-01-06 10:18:39 +0100

    linter errors corrected

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -35,7 +35,7 @@ canvas.addEventListener('mousemove', (e) => {
 })
 
 function drawCircle (x, y) {
-  ctx.beginPath();
+  ctx.beginPath()
   ctx.arc(x, y, size, 0, Math.PI * 2)
   ctx.fillStyle = color
   ctx.fill()
